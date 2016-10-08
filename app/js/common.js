283a56e654c0dc96ec1ff6d91511189162eb053d
Sinnerion <a.dergachow@gmail.com>
2016-10-08 14:55:14 +0300

    Feature section finished; created .animate-up for transleting block by hover

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,22 @@
 $(document).ready(function(){
-    // Header
+    // Sections
+    $('.animate-up').mouseenter(function(){
+       $(this).css({
+        '-webkit-transform': 'translateY(-10px)',
+        '-moz-transform': 'translateY(-10px)',
+        '-ms-transform': 'translateY(-10px)',
+        '-o-transform': 'translateY(-10px)',
+        'transform': 'translateY(-10px)',
+       })
+    });
+    $('.animate-up').mouseleave(function(){
+        $(this).css({
+            '-webkit-transform': 'translateY(0)',
+            '-moz-transform': 'translateY(0)',
+            '-ms-transform': 'translateY(0)',
+            '-o-transform': 'translateY(0)',
+            'transform': 'translateY(0)',
+        })
+    });
 
 });
