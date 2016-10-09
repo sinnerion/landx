72c3a3b53a3aef98de171aaed65583870b706fae
Sinnerion <a.dergachow@gmail.com>
2016-10-09 15:27:02 +0300

    Video section finished

--- app/js/common.js
+++ app/js/common.js
@@ -1,22 +1,42 @@
 $(document).ready(function(){
-    // Sections
-    $('.animate-up').mouseenter(function(){
-       $(this).css({
-        '-webkit-transform': 'translateY(-10px)',
-        '-moz-transform': 'translateY(-10px)',
-        '-ms-transform': 'translateY(-10px)',
-        '-o-transform': 'translateY(-10px)',
-        'transform': 'translateY(-10px)',
-       })
-    });
-    $('.animate-up').mouseleave(function(){
-        $(this).css({
-            '-webkit-transform': 'translateY(0)',
-            '-moz-transform': 'translateY(0)',
-            '-ms-transform': 'translateY(0)',
-            '-o-transform': 'translateY(0)',
-            'transform': 'translateY(0)',
-        })
-    });
+    // Translate animations
+    if($(window).width() > 767) {
+        $('.animate-up').mouseenter(function(){
+            $(this).css({
+                '-webkit-transform': 'translateY(-10px)',
+                '-moz-transform': 'translateY(-10px)',
+                '-ms-transform': 'translateY(-10px)',
+                '-o-transform': 'translateY(-10px)',
+                'transform': 'translateY(-10px)',
+            })
+        });
+        $('.animate-up').mouseleave(function(){
+            $(this).css({
+                '-webkit-transform': 'translateY(0)',
+                '-moz-transform': 'translateY(0)',
+                '-ms-transform': 'translateY(0)',
+                '-o-transform': 'translateY(0)',
+                'transform': 'translateY(0)',
+            })
+        });
+        $('.animate-down').mouseenter(function(){
+            $(this).css({
+                '-webkit-transform': 'translateY(10px)',
+                '-moz-transform': 'translateY(10px)',
+                '-ms-transform': 'translateY(10px)',
+                '-o-transform': 'translateY(10px)',
+                'transform': 'translateY(10px)',
+            })
+        });
+        $('.animate-down').mouseleave(function(){
+            $(this).css({
+                '-webkit-transform': 'translateY(0)',
+                '-moz-transform': 'translateY(0)',
+                '-ms-transform': 'translateY(0)',
+                '-o-transform': 'translateY(0)',
+                'transform': 'translateY(0)',
+            })
+        });
+    }
 
 });
